1445c42dff358399cbd498c4b4002fcf2be3ae4b
Benzmovies799 <128473684+Benzmovies799@users.noreply.github.com>
2024-11-30 19:06:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")  #dhini walla anukunta bhai
+URL = environ.get("URL", "https://representative-lyssa-benzmovies-5ac2787e.koyeb.app/")  #dhini walla anukunta bhai edhi cehyala nenu andhukey avaledhu
 
 
 # Rename Info : If True Then Bot Rename File Else Not
