12c42d9dc5c8b5ec05620376c39a37faa4373a0d
Benzmovies799 <128473684+Benzmovies799@users.noreply.github.com>
2024-12-01 02:18:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "fatal-lauretta-benzmawa-f7e55450.koyeb.app/")
+URL = environ.get("URL", "automatic-tansy-benzmawa-54a057e9.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
